7a5e8ffa15a528bf061c2ecef32b2a79dc3f929d
SIlboudo <121822491+SIlboudo@users.noreply.github.com>
2024-10-20 12:59:05 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+const menu_items1 = document.getElementById("hoverItem1");
+const menu_items2 = document.getElementById("hoverItem2");
+const menu_items3 = document.getElementById("hoverItem3");
+const menu_items4 = document.getElementById("hoverItem4");
+const menu_items5 = document.getElementById("hoverItem5");
+const menu_items6 = document.getElementById("hoverItem6");
+const menu_items7 = document.getElementById("hoverItem7");
+const initialColor = menu_items1.style.backgroundColor;
+
+menu_items1.addEventListener("mouseover", function () {
+    menu_items1.style.backgroundColor = "orange";
+    menu_items1.style.transform = "scale(1)";
+})
+
+menu_items2.addEventListener("mouseover", function () {
+    menu_items2.style.backgroundColor = "orange";
+    menu_items2.style.transform = "scale(1)";
+})
+
+menu_items3.addEventListener("mouseover", function () {
+    menu_items3.style.backgroundColor = "orange";
+    menu_items3.style.transform = "scale(1)";
+})
+
+menu_items4.addEventListener("mouseover", function () {
+    menu_items4.style.backgroundColor = "orange";
+    menu_items4.style.transform = "scale(1)";
+})
+
+menu_items5.addEventListener("mouseover", function () {
+    menu_items5.style.backgroundColor = "orange";
+    menu_items5.style.transform = "scale(1)";
+})
+
+menu_items6.addEventListener("mouseover", function () {
+    menu_items6.style.backgroundColor = "orange";
+    menu_items6.style.transform = "scale(1)";
+})
+
+menu_items7.addEventListener("mouseover", function () {
+    menu_items7.style.backgroundColor = "orange";
+    menu_items7.style.transform = "scale(1)";
+})
+
+menu_items1.addEventListener("mouseout", function () {
+    menu_items1.style.backgroundColor = initialColor;
+    menu_items1.style.transform = "scale(2)";
+})
+
+
+menu_items2.addEventListener("mouseout", function () {
+    menu_items2.style.backgroundColor = initialColor;
+    menu_items2.style.transform = "scale(2)";
+})
+
+menu_items3.addEventListener("mouseout", function () {
+    menu_items3.style.backgroundColor = initialColor;
+    menu_items3.style.transform = "scale(2)";
+})
+
+menu_items4.addEventListener("mouseout", function () {
+    menu_items4.style.backgroundColor = initialColor;
+    menu_items4.style.transform = "scale(2)";
+})
+
+menu_items4.addEventListener("mouseout", function () {
+    menu_items4.style.backgroundColor = initialColor;
+    menu_items4.style.transform = "scale(2)";
+})
+
+menu_items5.addEventListener("mouseout", function () {
+    menu_items5.style.backgroundColor = initialColor;
+    menu_items5.style.transform = "scale(2)";
+})
+
+menu_items6.addEventListener("mouseout", function () {
+    menu_items6.style.backgroundColor = initialColor;
+    menu_items6.style.transform = "scale(2)";
+})
+
+menu_items7.addEventListener("mouseout", function () {
+    menu_items7.style.backgroundColor = initialColor;
+    menu_items7.style.transform = "scale(2)";
+})
